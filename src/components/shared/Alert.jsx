e6af212021dadc23487d079f02df8a403c1b9a3e
Scott Mathson <smathson@fieldlevel.com>
2021-01-05 16:27:00 -0800

    Remove emotion, add css modules

---
+++ src/components/shared/Alert.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cx from 'classnames';
+
+import styles from './Alert.module.scss';
+
+const Alert = ({ type, children }) => {
+    const className = cx(styles.Alert, type === 'success' && styles.success, type === 'critical' && styles.critical);
+
+    return <div className={className}>{children}</div>;
+};
+
+export { Alert };
